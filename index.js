5f0b7e2184e282712ef2ca65bea6cb4be18f308b
Mathis <mathis.internicola@gmail.com>
2023-11-24 10:04:25 +0100

    Boutons marche !
    4 boutons de ville, en cours de personnalisation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // index.js
 import axios from 'axios';
 
+const cities = {
+    'Saint-Étienne': { lat: 45.4339, lon: 4.39 },
+    'Lyon': { lat: 45.75, lon: 4.85 },
+    'Marseille': { lat: 43.2965, lon: 5.37 },
+    'Paris': { lat: 48.8566, lon: 2.3522 }
+};
+
 const getWeatherForecast = async (latitude, longitude) => {
     const apiUrl = `https://api.open-meteo.com/v1/meteofrance?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin`;
 
@@ -52,23 +59,9 @@ const updateUI = (hourlyWeatherData) => {
     }
 };
 
+// Fonction pour utiliser Nominatim pour obtenir les coordonnées de la ville
 const getCoordinates = async (city) => {
-    const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
-    const nominatimUrl = `${corsAnywhereUrl}https://nominatim.openstreetmap.org/search/${encodeURIComponent(city)}?format=json`;
-
-    try {
-        const response = await axios.get(nominatimUrl);
-
-        if (response.data.length > 0) {
-            const { lat, lon } = response.data[0];
-            return { lat, lon };
-        } else {
-            throw new Error('Coordonnées introuvables pour la ville spécifiée:', city);
-        }
-    } catch (error) {
-        console.error('Erreur lors de la récupération des coordonnées:', error);
-        throw error;
-    }
+    return cities[city];
 };
 
 document.getElementById('location-form').addEventListener('submit', async function (event) {
@@ -85,13 +78,6 @@ document.getElementById('location-form').addEventListener('submit', async functi
     }
 });
 
-const updateCityName = (city) => {
-    const cityNameElement = document.getElementById('cityName');
-    if (cityNameElement) {
-        cityNameElement.innerText = city;
-    }
-};
-
 // Ajout de la fonction pour changer la ville
 const changeCity = async (city) => {
     try {
@@ -100,6 +86,13 @@ const changeCity = async (city) => {
         updateCityName(city);
     } catch (error) {
         console.error(error.message);
+    }
+};
+
+const updateCityName = (city) => {
+    const cityNameElement = document.getElementById('cityName');
+    if (cityNameElement) {
+        cityNameElement.innerText = city;
     }
 };
 
@@ -113,3 +106,57 @@ const updateClock = () => {
 };
 
 setInterval(updateClock, 1000);
+
+// Initialiser la météo pour Saint-Étienne au chargement de la page
+document.addEventListener('DOMContentLoaded', function () {
+    changeCity('Saint-Étienne');
+});
+
+// Ajouter des gestionnaires d'événements pour les boutons de ville
+document.getElementById('btnSaintEtienne').addEventListener('click', function () {
+    changeCity('Saint-Étienne');
+});
+
+document.getElementById('btnLyon').addEventListener('click', function () {
+    changeCity('Lyon');
+});
+
+document.getElementById('btnMarseille').addEventListener('click', function () {
+    changeCity('Marseille');
+});
+
+document.getElementById('btnParis').addEventListener('click', function () {
+    changeCity('Paris');
+});
+
+
+// Sélectionnez le conteneur des boutons
+const buttonsContainer = document.getElementById('buttons-container');
+
+// Sélectionnez chaque bouton par son ID
+const btnSaintEtienne = document.getElementById('btnSaintEtienne');
+const btnLyon = document.getElementById('btnLyon');
+const btnMarseille = document.getElementById('btnMarseille');
+const btnParis = document.getElementById('btnParis');
+
+// Appliquez les styles au conteneur des boutons
+buttonsContainer.style.display = 'flex'; // Utilisez flexbox pour aligner les boutons
+buttonsContainer.style.justifyContent = 'center'; // Centre les boutons horizontalement
+
+// Fonction pour appliquer les styles à chaque bouton
+const applyStyles = (button, bgColor) => {
+    button.style.width = '100px'; // Ajustez la largeur selon vos préférences
+    button.style.height = '100px'; // Ajustez la hauteur selon vos préférences
+    button.style.borderRadius = '50%'; // Rend le bouton rond
+    button.style.backgroundColor = bgColor;
+    button.style.cursor = 'pointer';
+    button.style.textAlign = 'center';
+    button.style.lineHeight = '100px'; // Centre le texte verticalement
+    button.style.margin = '0 10px'; // Ajoute une marge entre les boutons
+};
+
+// Appelez la fonction pour chaque bouton avec la couleur appropriée
+applyStyles(btnSaintEtienne, '#66c284'); // Vert pastel
+applyStyles(btnLyon, '#ffb347'); // Orange pastel
+applyStyles(btnMarseille, '#83c6e0'); // Bleu pastel
+applyStyles(btnParis, '#ffcccb'); // Rose pastel
